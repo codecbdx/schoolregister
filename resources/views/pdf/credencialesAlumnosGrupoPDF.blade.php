0d0911ddb86459a1d148c7647846729a90bd205a
Diego Bernal <jesusorantes8907@gmail.com>
2024-04-28 23:33:40 -0600

    update

--- resources/views/pdf/credencialesAlumnosGrupoPDF.blade.php
+++ resources/views/pdf/credencialesAlumnosGrupoPDF.blade.php
@@ -20,13 +20,14 @@
 
 <h4 class="text-center mt-4 mb-4">{{ $title }} - {{ $grupo }}</h4>
 
-@foreach($alumnos as $alumno)
-    <div style="width: 400px; height: 260px; margin: 0 auto; margin-bottom: 20px; border: 1px solid #000!important;">
+@foreach($alumnos as $index => $alumno)
+    <div
+        style="width: 400px; height: 260px; margin: 0 auto; margin-bottom: 20px; border: 1px solid #000!important;">
         <div
             style="width: 400px; height: 90px; margin: 0 auto; border-bottom: 1px solid #000!important; text-align: center;">
             <img src="{{ env('AWS_URL') }}{{ $configuracion->system_logo }}"
                  style="max-width: 63%; max-height: 63%; margin-top: 3px;">
-            <p style="width:80%; margin: 0 auto; text-align: center; font-size: 0.8em;">{{ $address }},
+            <p style="width: 80%; margin: 0 auto; text-align: center; font-size: 0.8em;">{{ $address }},
                 TEL. {{ $telephone }}.</p>
         </div>
 
@@ -37,7 +38,7 @@
             <p style="font-size: 0.9em;">{{ __('Validity') }}</p>
             <p style="font-size: 0.8em;"><b>{{ $alumno->vigencia }}</b></p>
         </div>
-        <div style="display:block; float: right; width: 63%;">
+        <div style="display: block; float: right; width: 63%;">
             <p style="font-size: 0.9em; margin-top: 20px;"><b>{{ __('Name') }}
                     :</b> {{ $alumno->nombre }} {{ $alumno->apellido_paterno }} {{ $alumno->apellido_materno }}</p>
             <p style="font-size: 0.9em;"><b>{{ __('CURP') }}:</b> {{ $alumno->curp }}</p>
@@ -48,6 +49,22 @@
             <p style="font-size: 0.9em;"><b>{{ __('Emergency Phone') }}:</b> {{ $alumno->telefono_emergencia }}</p>
         </div>
     </div>
+    @if((($index + 1) % 3 == 0))
+        <table style="width: 100%;">
+            <tr>
+                <td style="text-align: center;">
+                    <img src="{{ env('AWS_URL') }}{{ $configuracion->system_logo }}" style="width: 420px;">
+                </td>
+            </tr>
+            <tr>
+                <td style="text-align: center;">
+                    <p>{{ $address }}, TEL. {{ $telephone }}.</p>
+                </td>
+            </tr>
+        </table>
+
+        <h4 class="text-center mt-4 mb-4">{{ $title }} - {{ $grupo }}</h4>
+    @endif
 @endforeach
 
 </body>
